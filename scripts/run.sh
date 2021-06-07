bf83fb842db4b2dd951dae8509a4722c242b623e
Filip Rysavy <fil@siasky.net>
2021-06-07 11:04:14 +0200

    Quote bash vars

--- scripts/run.sh
+++ scripts/run.sh
@@ -18,33 +18,34 @@ END
 
 # Get wanted UID and GID of host user (not docker container user) from mounted
 # Sia data dir
-USER_ID=$(stat -c "%u" $SIA_DATA_DIR)
-GROUP_ID=$(stat -c "%g" $SIA_DATA_DIR)
+USER_ID=$(stat -c "%u" "$SIA_DATA_DIR")
+GROUP_ID=$(stat -c "%g" "$SIA_DATA_DIR")
 
 # Check if we want to run Sia as non-root user
-if [ "$(uname -m)" = "x86_64" ] && [ "$USER_ID:$GROUP_ID" != "0:0" ]; then
+if [ "$(uname -m)" = "x86_64" ] && [ "$USER_ID" != "0" ]; then
   # Used username and groupname (inside docker container)
   SIA_USER=user
+  SIA_GROUP="$SIA_USER"
 
   # Create group if not exists
-  cat /etc/group | grep "^$SIA_USER:" > /dev/null || addgroup --gid=$GROUP_ID $SIA_USER
+  cat /etc/group | grep "^$SIA_GROUP:" > /dev/null || addgroup --gid="$GROUP_ID" "$SIA_GROUP"
 
   # Set wanted group GID (in case user existed and wanted GID has changed)
-  groupmod -g $GROUP_ID $SIA_USER
+  groupmod -g "$GROUP_ID" "$SIA_GROUP"
 
   # Create user if not exists
   cat /etc/passwd | grep "^$SIA_USER:" > /dev/null || adduser \
     --disabled-password \
     --gecos "" \
-    --ingroup $SIA_USER \
-    --uid $USER_ID \
-    $SIA_USER
+    --ingroup "$SIA_GROUP" \
+    --uid "$USER_ID" \
+    "$SIA_USER"
 
   # Set wanted user UID (in case user existed and wanted UID has changed)
-  usermod -u $USER_ID $SIA_USER
+  usermod -u "$USER_ID" "$SIA_USER"
 
   # Change sia-data ownership recursively
-  chown -R $USER_ID:$GROUP_ID $SIA_DATA_DIR
+  chown -R "$USER_ID:$GROUP_ID" "$SIA_DATA_DIR"
 
   # Run as given user
 
@@ -56,10 +57,10 @@ if [ "$(uname -m)" = "x86_64" ] && [ "$USER_ID:$GROUP_ID" != "0:0" ]; then
   # We are using `su-exec` to start `siad` in order to change a user who
   # runs `siad` process (if we do not want to run as root).
   echo "Running Sia with permissions of a local user: $USER_ID:$GROUP_ID"
-  exec $SU_EXEC $USER_ID:$GROUP_ID $SIAD_CMD "$@"
+  exec "$SU_EXEC" "$USER_ID:$GROUP_ID" $SIAD_CMD "$@"
 else
   # Change sia-data ownership recursively back to root
-  chown -R root:root $SIA_DATA_DIR
+  chown -R root:root "$SIA_DATA_DIR"
 
   # Run as root
 
